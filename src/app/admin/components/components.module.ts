3216ea7d400cc5d1948598f21a276d5968658f43
FathGzl <fatihguzel3455@gmail.com>
2024-03-23 14:10:36 +0300

    modül import işlemi

--- src/app/admin/components/components.module.ts
+++ src/app/admin/components/components.module.ts
@@ -1,6 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthroizeMenuComponent } from './authroize-menu/authroize-menu.component';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { CustomersModule } from './customers/customers.module';
+import { RolesModule } from './roles/roles.module';
+import { UsersModule } from './users/users.module';
+import { AuthroizeMenuModule } from './authroize-menu/authroize-menu.module';
 
 
 
@@ -9,7 +16,14 @@ import { AuthroizeMenuComponent } from './authroize-menu/authroize-menu.componen
     AuthroizeMenuComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,    
+    AuthroizeMenuModule,
+    CustomersModule,
+    DashboardModule,    
+    OrdersModule,
+    ProductsModule,
+    RolesModule,
+    UsersModule
   ]
 })
 export class ComponentsModule { }
--- src/app/ui/components/components.module.ts
+++ src/app/ui/components/components.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductsModule } from './products/products.module';
+import { BasketsModule } from './baskets/baskets.module';
+import { HomeModule } from './home/home.module';
+import { LoginModule } from './login/login.module';
+import { PasswordResetModule } from './password-reset/password-reset.module';
+import { RegisterModule } from './register/register.module';
+import { UpdatePasswordModule } from './update-password/update-password.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    BasketsModule,
+    HomeModule,
+    LoginModule,
+    PasswordResetModule,
+    ProductsModule,
+    RegisterModule,
+    UpdatePasswordModule
+
   ]
 })
 export class ComponentsModule { }
